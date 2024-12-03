ddd9961d950c90a5614be5c5f668e5097362eadf
garci843 <garci843@purdue.edu>
2024-12-02 22:02:33 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Place your JavaScript in this file */
+// Simple scroll-to functionality for smooth navigation
+document.querySelectorAll('.navbar a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
